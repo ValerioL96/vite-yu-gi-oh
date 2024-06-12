946b345f79b5e4c8f7d7dc0cc1c622a348c569f1
ValerioL96 <v.lembo1396@gmail.com>
2024-06-12 17:23:43 +0200

    Created Vue 3 project with Vite

---
+++ README.md
@@ -0,0 +1,2 @@
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Al caricamento della pagina, effettuate una chiama ajax all'API di Yu Gi Oh: https://db.ygoprodeck.com/api/v7/cardinfo.php e con i dati restituiti, stampate una card per ogni carta.
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+@use './styles/general.scss';
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
